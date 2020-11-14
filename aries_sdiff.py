2cbe41630dc052ca3172fcebdc0afabc942a68ea
Janus Brink <janus@salt.ac.za>
2020-11-14 18:34:26 +0200

    Updated to include full variance error propagation

--- aries_sdiff.py
+++ aries_sdiff.py
@@ -8,6 +8,31 @@ import pylab as pl
 
 from aries_roi import roi
 
+#like st.sigmaclip, but duplicates operation on noise array "n" as well.
+def sigmaclip_n(
+        a,
+        n,
+        low,
+        high,
+        ):
+
+    mean_a = np.mean(a)
+    std_a = np.std(a)
+
+    c = np.arange(0)
+    cn = np.array([])
+    for i in range(np.shape(a)[0]):
+        if (a[i] > (mean_a - std_a*low) and  a[i] < (mean_a + std_a*high)):
+            c = np.append(c,a[i])
+            cn = np.append(cn,n[i])
+
+    return c, cn
+
+# determine ratio between two spectra
+# takes two image files and extracts spectra, subtracting the median of a beckground region for each
+# signal is summed over the spatial 'height' (2 x dy), and the mean is computed per spectal bin
+# (specified by xsum)
+# shot noise error propagation is maintained throughout
 def sdiff(
         afile,
         bfile,
@@ -33,43 +58,50 @@ def sdiff(
     bg2 = bg2
     xbin = xsum
 
-    # extract signal
+    # extract signal - spatial binningl
     aspec = (accd.data[y1:y2,:] - np.median(accd.data[bg1:bg2,:], axis=0)).sum(axis=0)
     bspec = (bccd.data[y1:y2,:] - np.median(bccd.data[bg1:bg2,:], axis=0)).sum(axis=0)
+    # error propagation
+    avar = (accd.data[y1:y2,:]+np.median(accd.data[bg1:bg2,:], axis=0)*(bg2-bg1)/np.square(bg2-bg1)).sum(axis=0)
+    bvar = (bccd.data[y1:y2,:]+np.median(bccd.data[bg1:bg2,:], axis=0)*(bg2-bg1)/np.square(bg2-bg1)).sum(axis=0)
+
+    if (xbin > 1):
+        # apply spectral binning
+        abin = aspec.reshape(-1,xbin)
+        bbin = bspec.reshape(-1,xbin)
+        avbin = avar.reshape(-1,xbin)
+        bvbin = bvar.reshape(-1,xbin)
+
+        cols = abin.shape[0]
+        a = []
+        av = []
+        for x in np.arange(cols):
+            k, kn = sigmaclip_n(abin[x], avbin[x], low=3.0, high=3.0)
+            a.extend([k])
+            av.extend([kn])
+
+        cols = bbin.shape[0]
+        b = []
+        bv = []
+        for x in np.arange(cols):
+            k, kn = sigmaclip_n(bbin[x], bvbin[x], low=3.0, high=3.0)
+            b.extend([k])
+            bv.extend([kn])
+
+        aspec = np.array([np.mean(x) for x in a])
+        bspec = np.array([np.mean(x) for x in b])
+
+        #error propagation
+        avar = np.array([x.sum()/np.square(np.shape(x)[0]) for x in av])
+        bvar = np.array([x.sum()/np.square(np.shape(x)[0]) for x in bv])
+
+    # spectral ratio
     rspec = bspec/aspec
+    # error propagation
+    rvar = np.square(rspec)*(avar/np.square(aspec) + bvar/np.square(bspec))
 
-    # apply spectral binning
-    abin = aspec.reshape(-1,xbin)
-    bbin = bspec.reshape(-1,xbin)
-    rbin = rspec.reshape(-1,xbin)
-
-    # estimate variance from counts per bin
-    # sigma-clip data in each bin for variance calc
-    #v = np.apply_along_axis(st.sigmaclip, 1, abin, low=3.0, high=3.0)
-    #avar = np.array([np.var(x) for x in v[:,0]])
-    #v = np.apply_along_axis(st.sigmaclip, 1, bbin, low=3.0, high=3.0)
-    #bvar = np.array([np.var(x) for x in v[:,0]])
-
-    # estimate variance from ratio per bin
-    c = np.apply_along_axis(st.sigmaclip, 1, rbin, low=3.0, high=3.0)
-    rvar = 2.0/xbin*np.array([np.var(x) for x in c[:,0]])
-
-    # perform sigma clipping on bins
-    c = np.apply_along_axis(st.sigmaclip, 1, abin, low=3.0, high=3.0)
-    aspec = np.array([np.mean(x) for x in c[:,0]])
-    c = np.apply_along_axis(st.sigmaclip, 1, bbin, low=3.0, high=3.0)
-    bspec = np.array([np.mean(x) for x in c[:,0]])
-    #aspec = abin.mean(axis=1)
-    #bspec = bbin.mean(axis=1)
-    rspec = bspec/aspec
-
-    # extract variance
-#    accdv = np.nan_to_num(accd.uncertainty.array)
-#    bccdv = np.nan_to_num(bccd.uncertainty.array)
-#    avar = accdv[y1:y2,:].sum(axis=0)
-#    bvar = bccdv[y1:y2,:].sum(axis=0)
-#    avar = avar.reshape(-1,xbin).mean(axis=1)
-#    bvar = bvar.reshape(-1,xbin).mean(axis=1)
+    #print rspec
+    #print np.sqrt(rvar)
 
     xarr = np.arange(len(aspec))
     warr = (wc + xbin/2) + dw*xbin*xarr
@@ -77,7 +109,9 @@ def sdiff(
     print 'ave = %f %%' % (rspec.mean()*100.0)
     print 'stdev = %f %%' % (rspec.std()*100.0)
     print 'range = %f %%' % ((rspec.max() - rspec.min())*100.0)
-    #print 'S/N = %f / %f = %f' % (aspec.mean(), avar.mean(), aspec.mean()/avar.mean())
+    print 'S/N(a) = %f / %f = %f' % (aspec.mean(), np.sqrt(avar).mean(), aspec.mean()/np.sqrt(avar).mean())
+    print 'S/N(b) = %f / %f = %f' % (bspec.mean(), np.sqrt(bvar).mean(), bspec.mean()/np.sqrt(bvar).mean())
+    print 'S/N(r) = %f / %f = %f' % (rspec.mean(), np.sqrt(rvar).mean(), rspec.mean()/np.sqrt(rvar).mean())
 
     if save:
         oarr = np.array([warr, aspec, bspec, rspec, rvar]).T
@@ -96,6 +130,7 @@ def sdiff(
         pl.ylabel('Diff', size='x-large')
         pl.subplot(313)
         pl.plot(warr, rspec*100.0)
+        pl.plot(warr, (rspec+np.sqrt(rvar))*100.0)
         pl.ylabel('Deviation (%)', size='x-large')
         pl.xlabel('Wavelength', size='x-large')
         pl.show()
